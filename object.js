94758fd197d3a0f336673c964da33fd79eb6bc86
Moideen Mashad <moideenmashad@gmail.com>
2024-05-28 23:50:56 +0530

    Updated

--- object.js
+++ object.js
@@ -3,4 +3,11 @@ let car = {
   brand: "BMW",
   modal: "M Competition",
   year: "2015",
+  engine: {
+    engin_size: "2.0 L",
+    cylinders: "Inline 4",
+    hp: "180 hp @ 5,000 rpm",
+    engine_type: "Gas",
+    torque: "200 lb-ft @ 1,250 rpm",
+  },
 };
