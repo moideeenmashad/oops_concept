0605d3d2b4311e6561e906d83cd50c8e0c7a1d96
Moideen Mashad <moideenmashad@gmail.com>
2024-05-30 00:08:10 +0530

    Updated

--- encapsulation.js
+++ encapsulation.js
@@ -4,14 +4,15 @@ class car {
     this.series = series;
     this.color = color;
   }
-  modal_year(year) {
+  modal_year(year, edition) {
     this.year = year;
+    this.edition = edition;
   }
   getDetails() {
-    return `The car is ${this.name} ${this.series} ${this.year} and its color is ${this.color}`;
+    return `The car is ${this.name} ${this.series} ${this.edition} ${this.year} and its color is ${this.color}`;
   }
 }
 
 let newCar = new car("BMW", "M3", "black");
-newCar.modal_year(2015);
+newCar.modal_year(2015,"Redbull");
 console.log(newCar.getDetails());
